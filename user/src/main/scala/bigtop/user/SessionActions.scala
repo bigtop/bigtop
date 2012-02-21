0aac14ae0a02e76bdfd984d76131c0ca8c13035a
Noel Welsh <noel@untyped.com>
2012-02-21 12:40:35 +0000

    Refactor session actions into one file. Makes it easier to work with the code.

--- user/src/main/scala/bigtop/user/SessionActions.scala
+++ user/src/main/scala/bigtop/user/SessionActions.scala
@@ -1,5 +1,46 @@
 package bigtop
 package user
 
+import akka.dispatch.{Future, Promise}
+import bigtop.json.{JsonWriter, JsonFormatters}
+import bigtop.util.Uuid
+import blueeyes.core.service.ServiceContext
+import blueeyes.persistence.mongo._
+import blueeyes.json.JsonAST._
+import blueeyes.json.JsonDSL._
+import com.twitter.util.{LruMap,SynchronizedLruMap}
+import net.lag.configgy.ConfigMap
+import net.lag.logging.Logger
+import scala.collection.mutable.HashMap
+
 trait SessionActions[U <: User] extends SessionCreate[U]
     with SessionRead[U]
+
+
+trait SessionCreate[U <: User] extends UserTypes[U] {
+
+  def core: SessionCore[U]
+
+  def userActions: UserActions[U]
+
+  def create(username: String, password: String): SessionValidation =
+    for {
+      user <- userActions.login(username, password)
+    } yield {
+      val id = Uuid.create()
+      val session = Session(id, user, new HashMap[String, JValue]())(userActions.core.serializer)
+      core.store.create(id, session)
+      session
+    }
+
+}
+
+
+trait SessionRead[U <: User] extends UserTypes[U] {
+
+  def core: SessionCore[U]
+
+  def read(id: Uuid): SessionValidation =
+    core.store.read(id)
+
+}
