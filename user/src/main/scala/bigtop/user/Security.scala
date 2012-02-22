00f5b86ce694aad58463f75431e5bdc792687dff
Noel Welsh <noel@untyped.com>
2012-02-22 17:00:57 +0000

    Implement (but not test, yet) a cookie authentication method. Required rejigging the type signatures for the security methods.

--- user/src/main/scala/bigtop/user/Security.scala
+++ user/src/main/scala/bigtop/user/Security.scala
@@ -3,23 +3,24 @@ package user
 
 import blueeyes.core.http.HttpRequest
 import bigtop.problem.Problem
-import bigtop.concurrent.FutureValidation
+import bigtop.concurrent._
 import scalaz.Validation
 
 trait Authenticated[U <: User] {
 
-  /** Return the user this request is authenticated as, if it is authenticated as a user. It's not an error to not be authenticated, so the result is an option not a Validation. */
-  def authenticated[A](request: HttpRequest[A]): Option[U]
+  /** Return the user this request is authenticated as, if it is authenticated as a user. It's not an error to not be authenticated, so the user is an option. We have a validation as other errors can occur. */
+  def authenticated[A](request: HttpRequest[A]): FutureValidation[Problem,Option[U]]
 
 }
 
-trait Authorized[U <: User] extends Authenticated[U] {
+trait Authorized[U <: User] extends Authenticated[U] with FutureImplicits {
 
-  def authorized[A,B](request: HttpRequest[A], condition: Option[U] => Validation[Problem,Option[U]])(f: Option[U] => B) = {
-    val user = authenticated(request)
-     for {
-       u <- condition(user)
-     } yield f(u)
+  def authorized[A,B](request: HttpRequest[A], condition: Option[U] => Validation[Problem,Option[U]])(f: Option[U] => FutureValidation[Problem, B]) = {
+    for {
+      user1    <- authenticated(request)
+      user2    <- condition(user1).fv
+      response <- f(user2)
+    } yield response
   }
 
 }
--- user/src/main/scala/bigtop/user/SessionCookieAuthenticator.scala
+++ user/src/main/scala/bigtop/user/SessionCookieAuthenticator.scala
@@ -1,10 +1,41 @@
 package bigtop
 package user
 
-import blueeyes.core.http.HttpRequest
+import blueeyes.core.http._
+import blueeyes.core.http.HttpHeaders._
+import bigtop.util.Uuid
+import bigtop.concurrent._
+import bigtop.problem._
+import bigtop.problem.Problems._
+import scalaz.Validation
+import scalaz.std.option.optionSyntax._
+import scalaz.syntax.validation._
 
-trait SessionCookieAuthenticator[U <: User] extends Authenticated[U] {
+trait SessionCookieAuthenticator[U <: User] extends Authenticated[U]
+    with HttpHeaderImplicits
+    with FutureImplicits
+{
 
-  def authenticated[A](request: HttpRequest[A]): Option[U]
+  def action: SessionRead[U]
+
+  val cookieName = "session"
+
+  def sessionCookie[A](request: HttpRequest[A]): Option[HttpCookie] = {
+    val cookies = request.headers.header[Cookie].flatMap(_.cookies.headOption)
+    cookies.filter(_.name == cookieName).headOption
+  }
+
+  def authenticated[A](request: HttpRequest[A]): FutureValidation[Problem,Option[U]] = {
+    sessionCookie(request) map {
+      cookie =>
+        for {
+          uuid    <- Uuid.parse(cookie.cookieValue).toSuccess(
+            Client.malformedArgument("session",
+                                     "Session cookie did not contain a valid UUID")
+          ).fv
+          session <- action.read(uuid)
+        } yield (Some(session.user) : Option[U])
+    } getOrElse (None : Option[U]).success[Problem].fv
+  }
 
 }
