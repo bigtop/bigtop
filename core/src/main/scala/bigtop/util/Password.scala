b0e7fd2cc0e6489f38acf7d474be4ffab10ab8f6
Dave Gurnell <dave@untyped.com>
2012-06-22 11:38:07 +0100

    Added Password class

---
+++ core/src/main/scala/bigtop/util/Password.scala
@@ -0,0 +1,30 @@
+package bigtop
+package util
+
+import bigtop.problem._
+import scalaz._
+import scalaz.Scalaz._
+
+case class Password(val hash: String) {
+  def matches(password: String): Boolean = {
+    BCrypt.compare(password, hash)
+  }
+}
+
+object Password {
+  def fromPassword(password: String): Password = {
+    Password(BCrypt.hash(password))
+  }
+
+  // def fromHash(hash: String): Password = {
+  //   Password(hash)
+  // }
+
+  def parseHash(hash: String): Validation[Problem, Password] = {
+    if(BCrypt.isHash(hash)) {
+      Password(hash).success[Problem]
+    } else {
+      Problems.Server.malformed("bcryptHash", "not.a.hash").fail[Password]
+    }
+  }
+}
