3c0231450b9474d80d4e69f5db045e7d9af56a62
Dave Gurnell <dave@untyped.com>
2013-03-29 11:18:32 +0000

    JsonConfig: changed String path arguments to JPaths

--- core/src/main/scala/bigtop/json/JsonConfig.scala
+++ core/src/main/scala/bigtop/json/JsonConfig.scala
@@ -8,24 +8,24 @@ import scalaz._
 import scalaz.Scalaz._
 
 case class JsonConfig(val data: JValue = JObject.empty) {
-  def apply[T](key: String)(implicit reader: JsonReader[T]): T =
-    get(key) match {
+  def apply[T](path: JPath)(implicit reader: JsonReader[T]): T =
+    get(path) match {
       case Success(value)  => value
       case Failure(errors) => throw JsonException(errors)
     }
 
-  def get[T](key: String)(implicit reader: JsonReader[T]): JsonValidation[T] =
-    data.get(key) match {
-      case JNothing => JsonErrors.Missing(key).fail[T]
+  def get[T](path: JPath)(implicit reader: JsonReader[T]): JsonValidation[T] =
+    data.get(path) match {
+      case JNothing => JsonErrors.Missing(path).fail[T]
       case json     => reader.read(json).fold(
                          succ = value => value.success[JsonErrors],
-                         fail = errors => errors.prefix(key).fail[T]
+                         fail = errors => errors.prefix(path).fail[T]
                        )
     }
 
-  def set[T](key: String, value: T)(implicit writer: JsonWriter[T]): JsonConfig =
-    JsonConfig(data.set(key, writer.write(value)))
+  def set[T](path: JPath, value: T)(implicit writer: JsonWriter[T]): JsonConfig =
+    JsonConfig(data.set(path, writer.write(value)))
 
-  def remove[T](key: String): JsonConfig =
-    JsonConfig(data.set(key, JNothing).minimize.getOrElse(JObject.empty))
+  def remove[T](path: JPath): JsonConfig =
+    JsonConfig(data.set(path, JNothing).minimize.getOrElse(JObject.empty))
 }
