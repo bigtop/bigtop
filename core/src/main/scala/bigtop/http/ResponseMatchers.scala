43024fd63f1a025aadfdb6d71bda4cd6f1d60104
Dave Gurnell <dave@untyped.com>
2013-04-18 18:14:16 +0100

    Revamped ResponseMatchers

--- core/src/main/scala/bigtop/http/ResponseMatchers.scala
+++ core/src/main/scala/bigtop/http/ResponseMatchers.scala
@@ -1,56 +1,62 @@
 package bigtop
 package http
 
+import bigtop.json._
 import blueeyes.json.JsonAST.JValue
-import blueeyes.core.http.{HttpStatus, HttpResponse, MimeTypes}
-import blueeyes.core.http.HttpStatusCodes._
+import blueeyes.core.http._
+import blueeyes.core.http.HttpStatusCodes
 import org.specs2.Specification
 import org.specs2.matcher._
 import bigtop.json.JsonFormatters._
 import bigtop.problem._
 
-trait ResponseMatchers extends MustMatchers
-  with StandardMatchResults {
+trait ResponseMatchers extends MustMatchers with StandardMatchResults {
 
-  def beOk: Matcher[HttpResponse[JValue]] =
-    beLike {
-      case HttpResponse(status, _, Some(content), _) =>
-        status.code aka jsonString(content) mustEqual OK
+  private def beAny[T]: Matcher[T] =
+    beLike[T] { case _ => ok }
+
+  def beResponse[T](beTheCode: Matcher[HttpStatusCode], beTheResult: Matcher[T])(implicit reader: JsonReader[T]) =
+    beLike[HttpResponse[JValue]] {
+      case HttpResponse(HttpStatus(code, _), _, Some(json), _) =>
+        (code must beTheCode) and
+        (json.as[T].fold(
+          succ = { problem => problem must beTheResult },
+          fail = { errors  => ko("Failed to parse JSON: " + json + "\nparse errors: " + errors) }
+        ))
+
+      case response =>
+        ko("Response did not match the correct pattern: " + response)
     }
 
-  def beOk(expected: JValue): Matcher[HttpResponse[JValue]] =
-    beLike {
-      case HttpResponse(status, _, Some(content), _) =>
-        (jsonString(content) mustEqual jsonString(expected)) and
-        (status.code mustEqual OK)
-    }
+  // def beResponse[T](beTheResult: Matcher[T])(implicit reader: JsonReader[T]): Matcher[HttpResponse[JValue]] =
+  //   beResponse(beAny[HttpStatusCode], beTheResult)
 
-  def beOk(beExpected: Matcher[JValue]): Matcher[HttpResponse[JValue]] =
-    beLike {
-      case HttpResponse(status, _, Some(content), _) =>
-        (content aka jsonString(content) must beExpected) and
-        (status.code mustEqual OK)
-    }
+  def beProblemResponse(beTheProblem: Matcher[Problem]): Matcher[HttpResponse[JValue]] =
+    beResponse[Problem](beAny[HttpStatusCode], beTheProblem)
 
-  def beProblem(beExpected: Matcher[Problem]): Matcher[HttpResponse[JValue]] =
-    beLike {
-      case HttpResponse(status, _, Some(content), _) =>
-        content.as[Problem].fold(
-          succ = { problem => problem must beExpected },
-          fail = { problem => ko(problem.toString) }
-        )
-    }
+  def beOk(): Matcher[HttpResponse[JValue]] =
+    be200(beAny[JValue])
 
-  def jsonDesc(json: JValue, prefix: String = "") =
-    (ignored: String) => prefix + jsonString(json)
+  def be200(): Matcher[HttpResponse[JValue]] =
+    be200(beAny[JValue])
 
-  def jsonString(desc: String, json: JValue): String =
-    desc + ": " + jsonString(json)
+  def be200[T](expected: T)(implicit reader: JsonReader[T]): Matcher[HttpResponse[JValue]] =
+    be200[T](beEqualTo(expected))
 
-  def jsonString(json: JValue): String = {
-    import blueeyes.json.Printer._
-    compact(render(json))
-  }
+  def be200[T](beTheResult: Matcher[T])(implicit reader: JsonReader[T]): Matcher[HttpResponse[JValue]] =
+    beResponse[T](beEqualTo(HttpStatusCodes.OK), beTheResult)
+
+  def be400(beTheProblem: Matcher[Problem]): Matcher[HttpResponse[JValue]] =
+    beResponse(beEqualTo(HttpStatusCodes.BadRequest), beTheProblem)
+
+  def be403(beTheProblem: Matcher[Problem]): Matcher[HttpResponse[JValue]] =
+    beResponse(beEqualTo(HttpStatusCodes.Forbidden), beTheProblem)
+
+  def be404(beTheProblem: Matcher[Problem]): Matcher[HttpResponse[JValue]] =
+    beResponse(beEqualTo(HttpStatusCodes.NotFound), beTheProblem)
+
+  def be500(beTheProblem: Matcher[Problem]): Matcher[HttpResponse[JValue]] =
+    beResponse(beEqualTo(HttpStatusCodes.InternalServerError), beTheProblem)
 
 }
 
